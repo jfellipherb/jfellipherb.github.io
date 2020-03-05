288663e1569b63dd1eb38cfb249c467625113a85
Cleverton Inacio <clev.inaciO@gmail.com>
2020-03-05 13:35:10 -0300

    Altera forma de envio de email

--- contact.php
+++ contact.php
@@ -12,21 +12,21 @@ $body_message .= 'E-mail: '.$field_email."\n";
 $body_message .= 'Message: '.$field_message;
 
 $headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
+$headers = 'Reply-To: '.$field_email."\r\n";
 
 $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'contact_page.html';
+		window.location = 'index.html';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Message failed. Please, send an email to gordon@template-help.com');
-		window.location = 'contact_page.html';
+		window.location = 'index.html';
 	</script>
 <?php
 }
